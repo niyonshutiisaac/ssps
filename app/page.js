db0da17b38195b667fbeeab12984d6b706deeef0
Niyonshuti Isaac <niyonshutiisaac7@gmail.com>
2025-08-20 02:04:31 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
-import Navbar from "../components/Navbar";
-import HeroSection from "../components/HeroSection";
-import ComingSoonModal from "../components/ComingSoonModal";
-import Footer from "../components/Footer";
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import ComingSoonModal from "@/components/ComingSoonModal";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
